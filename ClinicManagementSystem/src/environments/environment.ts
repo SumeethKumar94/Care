e6b0784b8552d1ca272c7e564c3eb925c4c65363
sumeeth.s <sumeeth.s@SUMEETHKUMAR>
2022-03-07 10:47:59 +0530

    Hosting Commit

--- ClinicManagementSystem/src/environments/environment.ts
+++ ClinicManagementSystem/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production:true,
-  apiUrl: 'http://www.care.somee.com',
+  production:false,
+  apiUrl:'http://www.care.somee.com',
 };
 
 /*
